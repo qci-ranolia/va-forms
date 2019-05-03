8d25137e073b9c9ec3a4120835ecaefe0da87731
deepak-ranolia <d.ranolia92@gmail.com>
2019-05-03 19:00:29 +0530

    first commit to project

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -5,11 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
+  forms : any;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(){
+    this.forms = [
+      {
+        project_name:"Machine survey form",
+        questions:4
+      },
+      {
+        project_name:"Open Defication",
+        questions:10
+      },
+      {
+        project_name:"google toilet locator",
+        questions:6
+      },
+      {
+        project_name:"Swach bharat mission",
+        questions:2
+      }
+    ]
+  }
+
+  ngOnInit(){ }
+  
+  formQuestions(i){
+
   }
 
 }
--- src/app/home/response/response.component.ts
+++ src/app/home/response/response.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./response.component.scss']
 })
 export class ResponseComponent implements OnInit {
-
-  constructor() { }
+  responses:any
+  showDetails:boolean = true;
+  hideme=[]
+  constructor() {
+    this.responses = [
+      {
+        project_name:"Machine survey form",
+        project_submitted_by:"submitted",
+        project_submition_date:"10/12/2018",
+        project_info:{
+          info1:"info1",
+          info2:"info",
+          info3:"info",
+          info4:"info"
+        }
+      },
+      {
+        project_name:"Open Defication",
+        project_submitted_by:"submitted",
+        project_submition_date:"10/12/2018",
+        project_info:{
+          info1:"info2",
+          info2:"info",
+          info3:"info",
+          info4:"info"
+        }
+      },
+      {
+        project_name:"google toilet locator",
+        project_submitted_by:"submitted",
+        project_submition_date:"10/12/2018",
+        project_info:{
+          info1:"info3",
+          info2:"info",
+          info3:"info",
+          info4:"info"
+        }
+      },
+      {
+        project_name:"Swach bharat mission",
+        project_submitted_by:"submitted",
+        project_submition_date:"10/12/2018",
+        project_info:{
+          info1:"info4",
+          info2:"info",
+          info3:"info",
+          info4:"info"
+        }
+      }
+    ]
+  }
 
   ngOnInit() {
   }
-
+  
 }
