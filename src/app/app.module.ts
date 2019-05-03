670a2f7954493fc2b4123b567b4232ba63085ba2
Durgesh Singh <singh.durgesh5@gmail.com>
2019-05-03 12:08:12 +0530

    html css work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { routes } from './app.routes';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { BackupcomponentComponent } from './home/backupcomponent/backupcomponent.component';
-import { FormComponent } from './home/form/form.component';
-import { ChatComponent } from './home/chat/chat.component';
-import { ResponseComponent } from './home/response/response.component';
-import { GraphicComponent } from './home/graphic/graphic.component';
+import { routes } from "./app.routes";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { BackupcomponentComponent } from "./home/backupcomponent/backupcomponent.component";
+import { FormComponent } from "./home/form/form.component";
+import { ChatComponent } from "./home/chat/chat.component";
+import { ResponseComponent } from "./home/response/response.component";
+import { GraphicComponent } from "./home/graphic/graphic.component";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { GraphicComponent } from './home/graphic/graphic.component';
     GraphicComponent
   ],
   imports: [
+    MatTabsModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
@@ -34,4 +36,4 @@ import { GraphicComponent } from './home/graphic/graphic.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
