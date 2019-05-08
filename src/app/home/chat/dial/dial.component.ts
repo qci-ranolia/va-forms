63579fa8a31e4eda80091455af307095e17d661e
Durgesh Singh <singh.durgesh5@gmail.com>
2019-05-08 16:33:36 +0530

    all Ui work done

--- src/app/home/chat/dial/dial.component.ts
+++ src/app/home/chat/dial/dial.component.ts
@@ -1,98 +1,125 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from "@angular/core";
 import { MatBottomSheet } from "@angular/material";
 import { PopUpComponent } from "../../../pop-up/pop-up.component";
 import { ProjectService } from "../../../service/ProjectService";
-import { OpentokService } from '../../../service/Opentok.service';
-import * as OT from '@opentok/client';
+import { OpentokService } from "../../../service/Opentok.service";
+import * as OT from "@opentok/client";
 
 @Component({
-  selector: 'app-dial',
-  templateUrl: './dial.component.html',
-  styleUrls: ['./dial.component.scss']
+  selector: "app-dial",
+  templateUrl: "./dial.component.html",
+  styleUrls: ["./dial.component.scss"]
 })
 export class DialComponent implements OnInit {
-
-  @Input() user:any;
+  @Input() user: any;
   userToBeDialed: any;
   session: OT.Session;
   streams: Array<OT.Stream> = [];
   changeDetectorRef: ChangeDetectorRef;
-  connected : boolean = true;
+  connected: boolean = true;
 
-  constructor(private ref: ChangeDetectorRef, private opentokService: OpentokService, private bottomSheet: MatBottomSheet, private projectService: ProjectService) {
+  constructor(
+    private ref: ChangeDetectorRef,
+    private opentokService: OpentokService,
+    private bottomSheet: MatBottomSheet,
+    private projectService: ProjectService
+  ) {
     // this.bottomSheet.open(PopUpComponent);
-    this.userToBeDialed = projectService.userToBeDialed
-    console.log(this.userToBeDialed)
+    this.userToBeDialed = projectService.userToBeDialed;
+    console.log(this.userToBeDialed);
     this.changeDetectorRef = ref;
   }
 
-  ngOnInit () {
+  ngOnInit() {
+    document.body.className = "bodyActive";
+    this.opentokService
+      .initSession()
+      .then((session: OT.Session) => {
+        this.session = session;
 
-    this.opentokService.initSession().then((session: OT.Session) => {
-      this.session = session;
+        session.on("streamCreated", function(event) {
+          session.subscribe(event.stream);
 
-      session.on("streamCreated", function(event) {
-        session.subscribe(event.stream);
+          let tabElements1 = document.getElementsByClassName(
+            "OT_subscriber"
+          )[1] as HTMLElement;
+          tabElements1.style.display = "none";
+          let tabElements2 = document.getElementsByClassName(
+            "OT_subscriber"
+          )[2] as HTMLElement;
+          tabElements2.style.display = "none";
+          let tabElements3 = document.getElementsByClassName(
+            "OT_subscriber"
+          )[3] as HTMLElement;
+          tabElements3.style.display = "none";
+          let tabElements4 = document.getElementsByClassName(
+            "OT_subscriber"
+          )[4] as HTMLElement;
+          tabElements4.style.display = "none";
+        });
 
-        let tabElements1 = document.getElementsByClassName("OT_subscriber")[1] as HTMLElement;
-        tabElements1.style.display = 'none'
-        let tabElements2 = document.getElementsByClassName("OT_subscriber")[2] as HTMLElement;
-        tabElements2.style.display = 'none'
-        let tabElements3 = document.getElementsByClassName("OT_subscriber")[3] as HTMLElement;
-        tabElements3.style.display = 'none'
-        let tabElements4 = document.getElementsByClassName("OT_subscriber")[4] as HTMLElement;
-        tabElements4.style.display = 'none'
-
-
+        this.session.on("streamDestroyed", event => {
+          const idx = this.streams.indexOf(event.stream);
+          if (idx > -1) {
+            this.streams.splice(idx, 1);
+            this.changeDetectorRef.detectChanges();
+          }
+        });
+      })
+      .then(() => this.opentokService.connect())
+      .catch(err => {
+        console.error(err);
+        alert(
+          "Unable to connect. Make sure you have updated the config.ts file with your OpenTok details."
+        );
       });
-
-      this.session.on('streamDestroyed', (event) => {
-        const idx = this.streams.indexOf(event.stream);
-        if (idx > -1) {
-          this.streams.splice(idx, 1);
-          this.changeDetectorRef.detectChanges();
-        }
-      });
-
-    })
-    .then(() =>
-    this.opentokService.connect())
-    .catch((err) => {
-      console.error(err);
-      alert('Unable to connect. Make sure you have updated the config.ts file with your OpenTok details.');
-    });
-
   }
 
   ngAfterViewInit() {
     this.connected = true;
-    let tabElements = document.getElementsByClassName("OT_subscriber")[0] as HTMLElement;
+    let tabElements = document.getElementsByClassName(
+      "OT_subscriber"
+    )[0] as HTMLElement;
     tabElements.style.height = "100px !important";
-    tabElements.style.display = 'block'
-    console.log(tabElements.style.height)
+    tabElements.style.display = "block";
+    console.log(tabElements.style.height);
 
-    let tabElements1 = document.getElementsByClassName("OT_subscriber")[1] as HTMLElement;
-    tabElements1.style.display = 'none'
-    let tabElements2 = document.getElementsByClassName("OT_subscriber")[2] as HTMLElement;
-    tabElements2.style.display = 'none'
-    let tabElements3 = document.getElementsByClassName("OT_subscriber")[3] as HTMLElement;
-    tabElements3.style.display = 'none'
-    let tabElements4 = document.getElementsByClassName("OT_subscriber")[4] as HTMLElement;
-    tabElements4.style.display = 'none'
-
+    let tabElements1 = document.getElementsByClassName(
+      "OT_subscriber"
+    )[1] as HTMLElement;
+    tabElements1.style.display = "none";
+    let tabElements2 = document.getElementsByClassName(
+      "OT_subscriber"
+    )[2] as HTMLElement;
+    tabElements2.style.display = "none";
+    let tabElements3 = document.getElementsByClassName(
+      "OT_subscriber"
+    )[3] as HTMLElement;
+    tabElements3.style.display = "none";
+    let tabElements4 = document.getElementsByClassName(
+      "OT_subscriber"
+    )[4] as HTMLElement;
+    tabElements4.style.display = "none";
   }
 
   ngOnDestroy() {
-    console.log("called")
-    let tabElements = document.getElementsByClassName("OT_subscriber")[0] as HTMLElement;
-    if(tabElements)
-    tabElements.style.display = 'none'
+    console.log("called");
+    let tabElements = document.getElementsByClassName(
+      "OT_subscriber"
+    )[0] as HTMLElement;
+    if (tabElements) tabElements.style.display = "none";
   }
 
   endSession() {
-    this.opentokService.disconnect()
-    this.connected = false;
-    this.projectService.emitUserDial(false)
-  }
+    let result = window.confirm("you want to quit the chat...");
+    if (result) {
+      document.body.className = " ";
 
+      setTimeout(() => {
+        this.opentokService.disconnect();
+        this.connected = false;
+        this.projectService.emitUserDial(false);
+      }, 500);
+    }
+  }
 }
--- src/app/service/Opentok.service.ts
+++ src/app/service/Opentok.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
-import * as OT from '@opentok/client';
+import { Injectable } from "@angular/core";
+import * as OT from "@opentok/client";
 
 @Injectable()
 export class OpentokService {
-
   session: OT.Session;
   token: string;
-  API_KEY= '46319802';
-  SESSION_ID= '2_MX40NjMxOTgwMn5-MTU1NzI5MzE3NDcwM351QWc0OW81N0JvdG1qYmk3bm52bUREUVF-fg';
-  TOKEN= 'T1==cGFydG5lcl9pZD00NjMxOTgwMiZzaWc9NjRmYmU5MzUzZjM0NDc2MDE5ZTM2ZWU0ZTQ1NDNhOTlhZWMwYmQxODppbml0aWFsX2xheW91dF9jbGFzc19saXN0PSZub25jZT0xNDMxNjImZXhwaXJlX3RpbWU9MTU1NzM3OTU3NCZyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5qTXhPVGd3TW41LU1UVTFOekk1TXpFM05EY3dNMzUxUVdjME9XODFOMEp2ZEcxcVltazNibTUyYlVSRVVWRi1mZyZjcmVhdGVfdGltZT0xNTU3MjkzMTc0';
+  API_KEY = "46319802";
+  SESSION_ID =
+    "1_MX40NjMxOTgwMn5-MTU1NzMxMTMzNDAzMH5HaVNtU2h2Y2JQOEFVdmVEYTdweFpmQk1-fg";
+  TOKEN =
+    "T1==cGFydG5lcl9pZD00NjMxOTgwMiZzaWc9YmUxOWIyNjhmZWFkNmQ3NTU0NjM4ZDdjY2U2MTBkYTAxOTRkZmY5Zjpyb2xlPXB1Ymxpc2hlciZub25jZT0xMzczNjkmY3JlYXRlX3RpbWU9MTU1NzMxMTMzNCZleHBpcmVfdGltZT0xNTU3Mzk3NzM0JnNlc3Npb25faWQ9MV9NWDQwTmpNeE9UZ3dNbjUtTVRVMU56TXhNVE16TkRBek1INUhhVk50VTJoMlkySlFPRUZWZG1WRVlUZHdlRnBtUWsxLWZnJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
   // TOKEN= 'T1==cGFydG5lcl9pZD00NjMxOTgwMiZzaWc9YzRlY2Y4ZGM0MTAxM2FhZDkxMmVhOTI3MWJmZDM0MGIxNTYxNjA0NDpjcmVhdGVfdGltZT0xNTU3MjkzNjI2JnNlc3Npb25faWQ9Ml9NWDQwTmpNeE9UZ3dNbjUtTVRVMU56STVNekUzTkRjd00zNTFRV2MwT1c4MU4wSnZkRzFxWW1rM2JtNTJiVVJFVVZGLWZnJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9JmV4cGlyZV90aW1lPTE1NTczODAwMjYmcm9sZT1wdWJsaXNoZXImbm9uY2U9NDM0Mjc1';
-  constructor() { }
+  constructor() {}
 
   getOT() {
     return OT;
@@ -26,12 +27,12 @@ export class OpentokService {
 
   connect() {
     return new Promise((resolve, reject) => {
-      this.session.connect(this.token, (err) => {
+      this.session.connect(this.token, err => {
         if (err) {
-          console.log(err+"")
+          console.log(err + "");
           reject(err);
         } else {
-          console.log(this.session)
+          console.log(this.session);
           resolve(this.session);
         }
       });
@@ -39,14 +40,14 @@ export class OpentokService {
   }
 
   disconnect() {
-    console.log(this.session)
+    console.log(this.session);
 
-    if(this.session){
-      console.log("________________")
+    if (this.session) {
+      console.log("________________");
       this.session.disconnect();
       this.session = undefined;
-      console.log("Disconnected")
-      console.log("________________")
+      console.log("Disconnected");
+      console.log("________________");
     }
   }
 }
